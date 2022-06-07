e7dbd3df9c515588537d0c3d357386356c7a59ae
zhangzhengyang <1074385735@qq.com>
2022-06-07 10:21:29 +0800

    复习react脚手架知识

---
+++ react_staging/12-解决样式丢失的问题/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import {Route,Switch} from 'react-router-dom'
+import Home from './pages/Home' //Home是路由组件
+import About from './pages/About' //About是路由组件
+import Header from './components/Header' //Header是一般组件
+import MyNavLink from './components/MyNavLink'
+
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<div className="row">
+					<div className="col-xs-offset-2 col-xs-8">
+						<Header/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-2 col-xs-offset-2">
+						<div className="list-group">
+							<MyNavLink to="/atguigu/about">About</MyNavLink>
+							<MyNavLink to="/atguigu/home">Home</MyNavLink>
+						</div>
+					</div>
+					<div className="col-xs-6">
+						<div className="panel">
+							<div className="panel-body">
+								{/* 注册路由 */}
+								<Switch>
+									<Route path="/atguigu/about" component={About}/>
+									<Route path="/atguigu/home" component={Home}/>
+								</Switch>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
---
+++ react_staging/13-精准匹配与模糊匹配/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import {Redirect, Route, Switch} from 'react-router-dom'
+import Home from './pages/Home' //Home是路由组件
+import About from './pages/About' //About是路由组件
+import Header from './components/Header' //Header是一般组件
+import MyNavLink from './components/MyNavLink'
+
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<div className="row">
+					<div className="col-xs-offset-2 col-xs-8">
+						<Header/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-2 col-xs-offset-2">
+						<div className="list-group">
+							<MyNavLink to="/about">About</MyNavLink>
+							<MyNavLink to="/home/a/b">Home</MyNavLink>
+						</div>
+					</div>
+					<div className="col-xs-6">
+						<div className="panel">
+							<div className="panel-body">
+								{/* 注册路由 exact */}
+								<Switch>
+									<Route exact path="/about" component={About}/>
+									<Route exact path="/home" component={Home}/>
+									<Redirect to="/about"/>
+								</Switch>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
