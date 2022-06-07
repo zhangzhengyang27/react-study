c53beb64ad5e85a99dfb6254306d11e0d9946d7f
zhangzhengyang <1074385735@qq.com>
2022-06-07 12:30:31 +0800

    redux的使用

--- redux_test/08-react-redux开发者工具/src/redux/store.js
+++ redux_test/08-react-redux开发者工具/src/redux/store.js
@@ -1,15 +1,19 @@
-/* 
+/*
 	该文件专门用于暴露一个store对象，整个应用只有一个store对象
 */
 
 //引入createStore，专门用于创建redux中最为核心的store对象
 import {createStore,applyMiddleware,combineReducers} from 'redux'
+
 //引入为Count组件服务的reducer
 import countReducer from './reducers/count'
+
 //引入为Count组件服务的reducer
 import personReducer from './reducers/person'
+
 //引入redux-thunk，用于支持异步action
 import thunk from 'redux-thunk'
+
 //引入redux-devtools-extension
 import {composeWithDevTools} from 'redux-devtools-extension'
 
@@ -19,5 +23,5 @@ const allReducer = combineReducers({
 	rens:personReducer
 })
 
-//暴露store 
+//暴露store
 export default createStore(allReducer,composeWithDevTools(applyMiddleware(thunk)))
