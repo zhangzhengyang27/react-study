ae933594a98304b6dd5a922fd89b9fb30669bc1c
zhangzhengyang <1074385735@qq.com>
2022-06-07 12:21:52 +0800

    redux的使用

--- redux_test/05-react-redux的基本使用/src/containers/Count/index.jsx
+++ redux_test/05-react-redux的基本使用/src/containers/Count/index.jsx
@@ -10,16 +10,17 @@ import {
 //引入connect用于连接UI组件与redux
 import {connect} from 'react-redux'
 
-/* 
+/*
 	1.mapStateToProps函数返回的是一个对象；
 	2.返回的对象中的key就作为传递给UI组件props的key,value就作为传递给UI组件props的value
 	3.mapStateToProps用于传递状态
 */
 function mapStateToProps(state){
+	debugger
 	return {count:state}
 }
 
-/* 
+/*
 	1.mapDispatchToProps函数返回的是一个对象；
 	2.返回的对象中的key就作为传递给UI组件props的key,value就作为传递给UI组件props的value
 	3.mapDispatchToProps用于传递操作状态的方法
