81fd6bdf9db951b0f47aab464cdb5bf76c91691a
zhangzhengyang <1074385735@qq.com>
2022-06-07 10:46:26 +0800

    复习react脚手架知识

--- react_staging/17-向路由组件传递state参数/src/App.jsx
+++ react_staging/17-向路由组件传递state参数/src/App.jsx
@@ -17,12 +17,6 @@ export default class App extends Component {
 				<div className="row">
 					<div className="col-xs-2 col-xs-offset-2">
 						<div className="list-group">
-
-							{/* 原生html中，靠<a>跳转不同的页面 */}
-							{/* <a className="list-group-item" href="./about.html">About</a>
-							<a className="list-group-item active" href="./home.html">Home</a> */}
-
-							{/* 在React中靠路由链接实现切换组件--编写路由链接 */}
 							<MyNavLink to="/about">About</MyNavLink>
 							<MyNavLink to="/home">Home</MyNavLink>
 						</div>
