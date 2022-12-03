a25a5e5b4cfecfdebfe32407dca24ef3e92f3ad0
zhangzhengyang <zhangzhengyang27@aliyun.com>
2022-12-03 20:38:17 +0800

    redux 简单案例

--- redux_test/src/pages/2-redux/index.jsx
+++ redux_test/src/pages/2-redux/index.jsx
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 //引入store，用于获取redux中保存状态
 import store from '../../redux/store'
 //引入actionCreator，专门用于创建action对象
-import {createIncrementAction,createDecrementAction} from '../../redux/count_action'
+import {
+    createIncrementAction,
+    createDecrementAction,
+    createIncrementAsyncAction,
+    createDecrementAsyncAction
+} from '../../redux/count_action'
 
 export default class Count extends Component {
 
-    state = {carName:'奔驰c63'}
+    state = {carName: '奔驰c63'}
 
     /* componentDidMount(){
         //检测redux中状态的变化，只要变化，就调用render
@@ -16,29 +21,36 @@ export default class Count extends Component {
     } */
 
     //加法
-    increment = ()=>{
+    increment = () => {
         const {value} = this.selectNumber
-        store.dispatch(createIncrementAction(value*1))
+        store.dispatch(createIncrementAction(value * 1))
     }
     //减法
-    decrement = ()=>{
+    decrement = () => {
         const {value} = this.selectNumber
-        store.dispatch(createDecrementAction(value*1))
+        store.dispatch(createDecrementAction(value * 1))
     }
     //奇数再加
-    incrementIfOdd = ()=>{
+    incrementIfOdd = () => {
         const {value} = this.selectNumber
         const count = store.getState()
-        if(count % 2 !== 0){
-            store.dispatch(createIncrementAction(value*1))
+        if (count % 2 !== 0) {
+            store.dispatch(createIncrementAction(value * 1))
         }
     }
     //异步加
-    incrementAsync = ()=>{
+    incrementAsync = () => {
         const {value} = this.selectNumber
-        setTimeout(()=>{
-            store.dispatch(createIncrementAction(value*1))
-        },500)
+        // setTimeout(()=>{
+        store.dispatch(createIncrementAsyncAction(value * 1, 500))
+        // },500)
+    }
+    //异步加
+    decrementAsync = () => {
+        const {value} = this.selectNumber
+        // setTimeout(()=>{
+        store.dispatch(createDecrementAsyncAction(value * 1, 500))
+        // },500)
     }
 
     render() {
@@ -50,10 +62,16 @@ export default class Count extends Component {
                     <option value="2">2</option>
                     <option value="3">3</option>
                 </select>&nbsp;
-                <button onClick={this.increment}>+</button>&nbsp;
-                <button onClick={this.decrement}>-</button>&nbsp;
-                <button onClick={this.incrementIfOdd}>当前求和为奇数再加</button>&nbsp;
-                <button onClick={this.incrementAsync}>异步加</button>&nbsp;
+                <button onClick={this.increment}>+</button>
+                &nbsp;
+                <button onClick={this.decrement}>-</button>
+                &nbsp;
+                <button onClick={this.incrementIfOdd}>当前求和为奇数再加</button>
+                &nbsp;
+                <button onClick={this.incrementAsync}>异步加</button>
+                &nbsp;
+                <button onClick={this.decrementAsync}>异步减</button>
+                &nbsp;
             </div>
         )
     }
